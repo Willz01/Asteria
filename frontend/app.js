5e434f05325c33c77937a16710102b6ec83c1e28
sam-mcmurray <smcmurray1126@gmail.com>
2022-02-18 15:08:25 +0100

    fixed reload

---
+++ frontend/app.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+const express = require('express')
+
+
+const app = express()
+app.use(express.json())
+const PORT = 5601
+
+app.use(express.static(path.join(__dirname, '../', 'frontend')))
+
+app.all("/*", (req, res) => {
+  res.sendFile(path.join(
+    __dirname, '../',
+    'frontend',
+    'index.html'
+  ))
+});
+
+
+// served pages '/start', '/bookings', '/signUp'
+
+
+app.listen(PORT, () => {
+  console.log(`Asteria server running @ PORT: ${PORT}`);
+  console.log('Start page served at http://127.0.0.1:5601/');
+})
